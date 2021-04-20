778e0f8cb509629bcc938bb44cbb62759fc0beea
Laura Martinez Klein <lmartinezklein@gmail.com>
2021-04-20 19:43:17 -0400

    completo y funcionando

---
+++ LMKFunction/LMKFunction/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿namespace LMKFunction.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Data
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Estudiante { get; set; }
+        [Required]
+        public int Temperatura { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+    }
+}
